00b3ccbfae4986a8ee5fac001647b2e5617091cc
Fordim <dvformos@gmail.com>
2019-03-11 16:01:07 +0300

    single-onclick method

--- js/style.js
+++ js/style.js
@@ -10,82 +10,21 @@ var draw = function(state){
     }
 };
 
-var updateGameTable = function (i) {
-    console.log(i);
-    return function () {
-        gameTable[i] = 'X';
-        draw(gameTable);
-        console.log(i);
-    };
-};
-
-
 
 var onPageLoaded = function(){
 
     var myElement = document.getElementById('myspan');
     myElement.innerText = 'newText';
 
-//    updateCards();
 
-    var gameBlocks = document.getElementsByClassName('gameBlock');
-
-    for (var i = 0; i < gameBlocks.length; i++){
-        gameBlocks[i].onclick = updateGameTable(i); //навешивание события
-    }
-
-    draw(gameTable);
-
-};
-
-
-/*
-var updateCards = function (){
-    var elements = document.querySelectorAll('.gameBlockLetter');
-
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        element.innerText = 'O';
-
-    }
-    console.log(element);
-};
-*/
-
-
-document.addEventListener('DOMContentLoaded', onPageLoaded);
-
-
-/*
-var a = function(number1, number2){
-    var summary = number1 * number2;
-    return summary
-};
-
-a(2,3);
-
-
-function a(number1){
-    var currentSum = number1;
-
-    function f(number2){
-        currentSum *= number2;
-        return f;
-    }
-
-    f.toString = function(){
-        return currentSum;
+    var gameBlocks = document.getElementById('gameBlocks');
+    gameBlocks.onclick = function (e) {
+        var index = Array.from(e.target.parentElement.children).indexOf(e.target);
+        gameTable[index] = '?';
+        draw(gameTable);
     };
 
-    return f;
-}
+    draw(gameTable);
+};
 
-a(2)(3);
-
-
-function multi(a) {
-  return function(b) {
-  	return a * b;
-  }
-}
-*/
+document.addEventListener('DOMContentLoaded', onPageLoaded);
