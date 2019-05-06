b222810f9bf44451ec64eb8ff59f30562127faa9
Fordim <dvformos@gmail.com>
2019-05-06 15:43:13 +0300

    Moved static content to separate public folder, for security reasons

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use(express.static('.'));
+app.use(express.static('public'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
